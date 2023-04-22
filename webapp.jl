344e19d638d8387c47b5841f8663d49b3148a9ca
Jaroslaw Piskorski <jaropis@zg.home.pl>
2023-04-22 13:39:38 +0200

    offset

--- webapp.jl
+++ webapp.jl
@@ -7,14 +7,21 @@ function request_handler(req)
     if req.method == "GET" && req.target == "/"
         return HTTP.Response(200, read("index.html"))
     elseif req.method == "POST" && req.target == "/calculate"
+        segment_length = 20000
+        y_offset = 1150
         filepath = "g068.EDF"
         edf_signal = read_edf(filepath)
         y_values = select_signal(edf_signal, 1)
         x_values = range(0, length(y_values) - 1, step=1)
         templat = template_search(250, y_values[1:20*250])
-        rs = locate_rs(y_values, templat, 0.96)
-        plot_results(y_values, rs, "new_plot.png", 1, 80000, (1600, 500))
-        return HTTP.Response(200, JSON3.write(Dict("x" => x_values, "y" => y_values)))
+        rs = locate_rs(y_values, templat, 0.96, 0.33)
+        pointsX = rs[rs.<=x_values[segment_length]]
+        return HTTP.Response(200, JSON3.write(
+            Dict(
+                "x" => x_values[1:segment_length],
+                "y" => y_values[1:segment_length],
+                "pointsX" => pointsX,
+                "pointsY" => y_values[pointsX] .+ y_offset)))
     else
         return HTTP.Response(404, "Not Found")
     end
