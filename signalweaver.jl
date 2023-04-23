6fb2d2035a378777aa323b0a4d94bdf2bbe08802
Jaroslaw Piskorski <jaropis@zg.home.pl>
2023-04-23 08:36:00 +0200

    module documentation added

--- signalweaver.jl
+++ signalweaver.jl
@@ -7,8 +7,8 @@ using Random
 using ImageFiltering
 using EDF
 export read_edf, template_search, locate_rs, plot_results, describe_edf_signal, select_signal
-function template_search(frequency, window)
-    """
+
+"""
     Template search.
 
     This function looks repeating shape.
@@ -20,6 +20,7 @@ function template_search(frequency, window)
     Returns:
     vector with autocorrelations within the window.
     """
+function template_search(frequency, window)
     if (isodd(frequency))
         error("frequency needs to be an even number")
     end
@@ -47,6 +48,18 @@ function template_search(frequency, window)
     return (template)
 end
 
+"""
+    locate_rs(ecg, template, detection_threshold, amplitude_threshold)
+
+Locates the positions of the maxima in the ECG signal.
+
+* `ecg`: ECG signal
+* `template`: Template signal to search for
+* `detection_threshold`: Correlation threshold for detecting matches
+* `amplitude_threshold`: Amplitude threshold for detecting matches
+
+Returns a vector containing the positions of the maxima.
+"""
 locate_rs = function (ecg, template, detection_threshold, amplitude_threshold)
     rs = []
     local_segment = fill(0, length(template))
@@ -86,6 +99,21 @@ locate_rs = function (ecg, template, detection_threshold, amplitude_threshold)
     return (rs)
 end
 
+"""
+    plot_results(window, good_maxima, filename, min, max, plot_size=(1000, 500), draw=true)
+
+Plots the results of the analysis.
+
+* `window`: The segment of the signal to be analyzed
+* `good_maxima`: Positions of maxima
+* `filename`: Name of the file to save the plot
+* `min`: Minimum index for the plot
+* `max`: Maximum index for the plot
+* `plot_size`: Size of the plot (optional, default: (1000, 500))
+* `draw`: Boolean indicating whether to draw the plot (optional, default: true)
+
+Returns a Plots plot object.
+"""
 function plot_results(window, good_maxima, filename, min, max, plot_size=(1000, 500), draw=true)
     """
     plot results
@@ -116,15 +144,43 @@ function plot_results(window, good_maxima, filename, min, max, plot_size=(1000, 
     return (p)
 end
 
+"""
+    read_edf(filepath)
+
+Reads an EDF file and returns the signals.
+
+* `filepath`: Path to the EDF file
+
+Returns a vector of EDF signals.
+"""
 function read_edf(filepath)
     data = EDF.read(filepath)
     data.signals
 end
 
+"""
+    select_signal(edf_signal, edf_signal_idx)
+
+Selects a specific signal from a collection of EDF signals.
+
+* `edf_signal`: Collection of EDF signals
+* `edf_signal_idx`: Index of the desired signal
+
+Returns the samples of the selected signal.
+"""
 function select_signal(edf_signal, edf_signal_idx)
     return ((edf_signal[edf_signal_idx]).samples)
 end
 
+"""
+    describe_edf_signal(edf_signal)
+
+Prints the headers of a collection of EDF signals.
+
+* `edf_signal`: Collection of EDF signals
+
+No return value; headers are printed to the console.
+"""
 function describe_edf_signal(edf_signal)
     idx = 1
     for sigdx in edf_signal
